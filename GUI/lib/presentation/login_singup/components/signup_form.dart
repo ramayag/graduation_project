172ceb5fdc565a5705d19bfc48d00f06827f4bfe
Ramayag <ramasyr56@gmail.com>
2023-05-26 15:04:48 +0300

    connect data

---
+++ GUI/lib/presentation/login_singup/components/signup_form.dart
@@ -0,0 +1,131 @@
+import 'dart:convert';
+
+import 'package:gui/core/utils/app_assets.dart';
+import 'package:gui/core/utils/constants.dart';
+import 'package:gui/core/utils/enums.dart';
+import 'package:gui/core/utils/responsive.dart';
+import 'package:gui/data/datasource/databasehelper.dart';
+import 'package:gui/presentation/dashboard/MenuAppController.dart';
+import 'package:gui/presentation/dashboard/dashboard_screen.dart';
+import 'package:gui/presentation/login_singup/components/edit_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gui/presentation/login_singup/screens/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+
+class SingupForm extends StatelessWidget {
+  final String loginType;
+  // final TextEditingController _nameController = new TextEditingController();
+  // final TextEditingController _emailController = new TextEditingController();
+  // final TextEditingController _passwordController = new TextEditingController();
+  const SingupForm({Key? key, required this.loginType}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(child:Stack(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: SvgPicture.asset(
+            AppAssets.upCircle,
+            fit: BoxFit.fill,
+            height: MediaQuery.of(context).size.height / 8,
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: SvgPicture.asset(
+            AppAssets.bottomCircle,
+            fit: BoxFit.fill,
+            height: MediaQuery.of(context).size.height / 8,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(
+              vertical: 30, horizontal: Responsive.isMobile(context) ? 16 : 48),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('App Name',
+                  style: Theme.of(context).textTheme.displayMedium),
+              Constants.verticalSpaceLarge(),
+              Text(loginType,
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall!
+                      .copyWith(fontSize: 32)),
+              Constants.verticalSpaceMedium(),
+              EditTextField(
+                  label: 'Name',
+                  hint: 'Enter your name',
+                  width: Responsive.isMobile(context)
+                      ? MediaQuery.of(context).size.width
+                      : MediaQuery.of(context).size.width / 4,
+                  controller: TextEditingController(),
+                  type: TextInputType.name,
+                onChanged: (value) {
+                  DatabaseHelper.name=value;
+                },
+              ),
+              EditTextField(
+                  label: 'Email',
+                  hint: 'Enter your email',
+                  width: Responsive.isMobile(context)
+                      ? MediaQuery.of(context).size.width
+                      : MediaQuery.of(context).size.width / 4,
+                  controller: TextEditingController(),
+                  type: TextInputType.name,
+                onChanged: (value) {
+                  DatabaseHelper.email=value;
+                },),
+              Constants.verticalSpaceMedium(),
+              EditTextField(
+                label: 'Password',
+                hint: 'Enter password',
+                width: Responsive.isMobile(context)
+                    ? MediaQuery.of(context).size.width
+                    : MediaQuery.of(context).size.width / 4,
+                controller: TextEditingController(),
+                type: TextInputType.visiblePassword,
+                onChanged: (value) {
+                  DatabaseHelper.password=value;
+                },
+              ),
+              Constants.verticalSpaceLarge(),
+              MaterialButton(
+                  padding:
+                      EdgeInsets.all(Responsive.isMobile(context) ? 10 : 20),
+                  minWidth: Responsive.isMobile(context)
+                      ? MediaQuery.of(context).size.width / 2
+                      : MediaQuery.of(context).size.width / 4,
+                  color: Theme.of(context).primaryColor,
+                  shape: Constants.kFilledButton,
+                  child: Text(loginType,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                            color: Colors.white,
+                          )),
+                  onPressed: () async {
+                    await DatabaseHelper.registerData();
+                    if (DatabaseHelper.status) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => MultiProvider(
+                          providers: [
+                            ChangeNotifierProvider(
+                              create: (context) => MenuAppController(),
+                            ),
+                          ],
+                          child: LoginScreen(loginType: LoginType.signIn),
+                        ),
+                      ));
+                    }
+                  }
+                  ),
+            ],
+          ),
+        ),
+      ],
+    ));
+  }
+}
